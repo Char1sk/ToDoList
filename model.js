c3b173b2de65ce59a444f0cda1a8068c7c482a29
Charisk <2553036255@qq.com>
2020-10-08 21:16:00 +0800

    classes shape done

---
+++ model.js
@@ -0,0 +1,19 @@
+var example1 = new Vue({
+    el: '.classes',
+    data: {
+      classes: [
+        { message: 'Foo' },
+        { message: 'Foo' },
+        { message: 'Foo' },
+        { message: 'Foo' },
+        { message: 'Foo' },
+        { message: 'Foo' },
+        { message: 'Foo' },
+        { message: 'Foo' },
+        { message: 'Foo' },
+        { message: 'Foo' },
+        { message: 'Foo' },
+        { message: 'Bar' }
+      ]
+    }
+})
